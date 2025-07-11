0e48d6a0a21c448f412aa97f147bc847b02e6765
mathpvx <7547@holbertonstudents.com>
2025-07-11 10:16:32 +0200

    teacher obj

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,19 @@
+// Define the Teacher interface
+export interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience? : number;
+  location: string;
+  [propName: string]: any;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
