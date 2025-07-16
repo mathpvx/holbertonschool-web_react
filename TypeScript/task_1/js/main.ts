d1b0b8428656add0b3773fe348319e727bfc3640
mathpvx <7547@holbertonstudents.com>
2025-07-16 17:51:21 +0200

    ad Teacher and directors interfaces, printTeacher function with type

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,12 +1,21 @@
-// Define the Teacher interface
-export interface Teacher {
+interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
-  yearsOfExperience? : number;
+  yearsOfExperience?: number;
   location: string;
   [propName: string]: any;
 }
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
 
 interface Directors extends Teacher {
   numberOfReports: number;
@@ -20,6 +29,8 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 
+console.log(director1);
+
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
@@ -27,3 +38,5 @@ interface printTeacherFunction {
 const printTeacher: printTeacherFunction = (firstName, lastName) => {
   return `${firstName.charAt(0)}. ${lastName}`;
 };
+
+console.log(printTeacher("John", "Doe"));
