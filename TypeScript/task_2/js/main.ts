8685e0fac50c3e0fb7fd29eaf0cbcd5124db3760
mathpvx <7547@holbertonstudents.com>
2025-07-16 17:55:48 +0200

    Add isDirector type guard and executeWork function based on employee role

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -45,6 +45,16 @@ function createEmployee(salary: number | string): Director | Teacher {
   return new Director();
 }
 
-console.log(createEmployee(200));
-console.log(createEmployee(1000));
-console.log(createEmployee('$500'));
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  return employee.workTeacherTasks();
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
