70b5fbb468689b8a819302195088486458c9314b
mathpvx <7547@holbertonstudents.com>
2025-07-28 14:32:05 +0200

    update notifs

--- react_intro/task_2/dashboard/src/Notifications.jsx
+++ react_intro/task_2/dashboard/src/Notifications.jsx
@@ -1,9 +1,27 @@
+import React from 'react';
 import './Notifications.css';
+import closeIcon from './assets/close-icon.png';
+import { getLatestNotification } from './utils';
 
 function Notifications() {
   return (
     <div className="notifications">
+      <button
+        style={{ position: 'absolute', top: 10, right: 10, background: 'none', border: 'none' }}
+        aria-label="Close"
+        onClick={() => console.log('Close button has been clicked')}
+      >
+        <img src={closeIcon} alt="close" style={{ height: '10px', width: '10px' }} />
+      </button>
       <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li
+          data-priority="urgent"
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        />
+      </ul>
     </div>
   );
 }
