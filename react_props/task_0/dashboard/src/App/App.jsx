a29e2e05b08d45c09f8024d49074dd5b1609785e
mathpvx <7547@holbertonstudents.com>
2025-07-29 10:51:07 +0200

    add files

--- react_props/task_0/dashboard/src/App/App.jsx
+++ react_props/task_0/dashboard/src/App/App.jsx
@@ -1,40 +1,22 @@
-import logo from './assets/holberton-logo.jpg';
-import './App.css';
-import { getCurrentYear, getFooterCopy } from '../utils/utils';
+import React, { Fragment } from 'react';
+import '../App/App.css';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
 import Notifications from '../Notifications/Notifications';
 
 function App() {
-
   return (
-    <>
-      <div className="root-notifications">
-        <Notifications />
+    <Fragment>
+      <div className="App">
+        <div className="root-notifications">
+          <Notifications />
+        </div>
+        <Header />
+        <Login />
+        <Footer />
       </div>
-
-      <div className="App-header">
-        <img src={logo} alt="holberton logo" className="App-logo" />
-        <h1>School dashboard</h1>
-      </div>
-
-      <div className="App-divider" />
-
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
-
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" />
-
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" />
-
-        <button>OK</button>
-      </div>
-
-      <div className="App-footer">
-        <div className="App-divider" />
-        <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
-      </div>
-    </>
+    </Fragment>
   );
 }
 
